3eef3e9ba39ea82ad4d855f8b0747733967ad060
Jake Henak <6320837+jakehenak@users.noreply.github.com>
2025-07-05 21:48:49 -0400

    Updated capability check.

--- setup.py
+++ setup.py
@@ -72,10 +72,11 @@ compute_capabilities = set()
 device_count = torch.cuda.device_count()
 for i in range(device_count):
     major, minor = torch.cuda.get_device_capability(i)
-    if major < 8:
+    if (major < 7) or (major == 7 and minor < 5):
         warnings.warn(f"skipping GPU {i} with compute capability {major}.{minor}")
         continue
     compute_capabilities.add(f"{major}.{minor}")
+
 
 nvcc_cuda_version = get_nvcc_cuda_version(CUDA_HOME)
 if not compute_capabilities:
